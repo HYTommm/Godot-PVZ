0b03b0e181fe48f9e9f92e2ea96d9ed14e354e35
HYTommm <3146776307@qq.com>
2025-04-16 18:45:02 +0800

    实现阳光的自然刷新

--- MainGame/Plants/Bullet.cs
+++ MainGame/Plants/Bullet.cs
@@ -5,7 +5,9 @@ public partial class Bullet : Node2D
 {
 	[Export]
 	public float ShadowPositionY = 0.0f; // 子弹的阴影位置
+
 	bool IsDisappear = false; // 这个变量用来判断子弹是否消失
+
 	GpuParticles2D GPUParticles; // 子弹的粒子效果
 	AudioStreamPlayer BulletSplatsSound = new AudioStreamPlayer(); // 子弹的爆炸声音
 	
@@ -21,36 +23,48 @@ public partial class Bullet : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		//子弹向右移动
-		Position += new Vector2(10 * (float)delta * 30, 0);
-		//判断子弹是否超出屏幕范围
-		if (GlobalPosition.X > 1000)
+		if (!IsDisappear)
 		{
-			QueueFree();
+			//子弹向右移动
+			Position += new Vector2(333 * (float)delta, 0);
+			//判断子弹是否超出屏幕范围
+			if (GlobalPosition.X > 1000)
+			{
+				QueueFree();
+			}
 		}
+		
 	}
 
 	async public void OnAreaEntered(Area2D area)
 	{
 		//GD.Print("Bullet collided with " + area.Name);
-		if (area.GetNode("..") is Zombie zombie)
+		
+
+		// 判断子弹是否击中僵尸
+		if (area.GetNode("..") is Zombie zombie && !IsDisappear) 
 		{
-			GetNode<Sprite2D>("Sprite2D").Visible = false;
-			GetNode<Sprite2D>("Shadow").Visible = false;
-			GetNode<Area2D>("./Sprite2D/Area2D").CallDeferred("set_monitoring", false);
-			//GD.Print("Bullet hit zombie");
+			IsDisappear = true; // 子弹消失
+			GetNode<Sprite2D>("Sprite2D").Visible = false; // 子弹不可见
+			GetNode<Sprite2D>("Shadow").Visible = false; // 子弹阴影不可见
+			//GetNode<Area2D>("./Area2D").CallDeferred("set_monitoring", false); // 停止检测子弹碰撞
+			GetNode<Area2D>("./Area2D").SetDeferred("monitoring", false); // 停止检测子弹碰撞
+
+
+			GD.Print("Bullet hit zombie"); 
 			//僵尸扣血
 			zombie.Hurt(Damage);
 			//zombie.Die();
 			//是sprite节点不可见
 			
-			IsDisappear = true;
-			GPUParticles.Emitting = true;
 			GPUParticles.OneShot = true;
+			GPUParticles.SetDeferred("emitting", true);
+			
 
-			//随机数
+			// 随机数
 			uint random = GD.Randi() % 3;
 			//GD.Print("Random number: " + random);
+			// 根据随机数播放不同的爆炸声音
 			switch (random)
 			{
 				case 0:
@@ -64,13 +78,18 @@ public partial class Bullet : Node2D
 					break;
 			}
 
+			// 设置爆炸声音的音量 
 			BulletSplatsSound.VolumeDb = -5;
-			
+			// 播放爆炸声音
 			BulletSplatsSound.Play();
 
-
+			// 延迟0.4秒后销毁子弹
 			await ToSignal(GetTree().CreateTimer(0.4), SceneTreeTimer.SignalName.Timeout);
 			QueueFree();
+		}
+		else
+		{
+			GD.Print("子弹没有击中僵尸！它可能击中了其他东西：" + area.Name);
 		}
 	}
 
